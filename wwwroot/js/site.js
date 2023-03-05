e9f6353395ea5464bcf3ef4f221bf159670ff5bc
Luke Obornik <93653855+LukeWhiskey@users.noreply.github.com>
2023-03-05 01:24:04 +0000

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(() => {
+    $('#nav').css('overflow-y', 'auto');
+    $('.open').click(() => {
+        $('.open').css('display', 'none');
+        $('.backimg').css('margin-right', '-300px');
+    });
+    $('.close').click(() => {
+        $('.open').css('display', 'flex');
+        $('.backimg').css('margin-right', '0px');
+    });
+});
